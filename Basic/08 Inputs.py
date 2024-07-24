34081feea5033c91db7c9f21108304feec507ddf
osadhaambawalage <osadhaambawalage@gmail.com>
2024-07-24 22:57:34 +0530

    bitwise shift , inputs and piority order

---
+++ Basic/08 Inputs.py
@@ -0,0 +1,9 @@
+x = input("Enter value for x "); # 5 
+print(x); # 5 
+print(type(x));
+
+
+age = int (input("Enter your age : "));
+print("Value of age ", age);
+print('Type of age variable is ',type(age));
+
